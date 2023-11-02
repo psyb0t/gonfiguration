ae440aaf2a0effe3013dbe1e9b62a050d7ca52e9
Ciprian Mandache <ciprian@51k.eu>
2023-11-03 00:30:56 +0200

    rm obsolete err

--- errors.go
+++ errors.go
@@ -2,7 +2,4 @@ package gonfiguration
 
 import "errors"
 
-var (
-	ErrParsingConfig  = errors.New("error parsing config")
-	ErrDefaultsNotSet = errors.New("defaults not set. use gonfiguration.SetDefaults")
-)
+var ErrParsingConfig = errors.New("error parsing config")
