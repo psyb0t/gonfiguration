fe7be9e549805e102b936fbcdca462d28bbe208f
Ciprian Mandache <ciprian@51k.eu>
2023-11-03 01:59:52 +0200

    add more test cases + fix

--- gonfiguration_test.go
+++ gonfiguration_test.go
@@ -1,6 +1,7 @@
 package gonfiguration_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -11,14 +12,14 @@ import (
 //nolint:funlen
 func TestParse(t *testing.T) {
 	type SimpleConfig struct {
-		Name   string  `mapstructure:"name"`
-		Age    int     `mapstructure:"age"`
-		Active bool    `mapstructure:"active"`
-		Height float64 `mapstructure:"height"`
+		Name   string  `mapstructure:"NAME"`
+		Age    int     `mapstructure:"AGE"`
+		Active bool    `mapstructure:"ACTIVE"`
+		Height float64 `mapstructure:"HEIGHT"`
 	}
 
 	type ComplexConfig struct {
-		Name string `mapstructure:"name"`
+		Name string `mapstructure:"NAME"`
 		Info struct {
 			Age    int
 			Height float64
@@ -26,12 +27,12 @@ func TestParse(t *testing.T) {
 	}
 
 	type PtrFieldConfig struct {
-		Name *string `mapstructure:"name"`
-		Age  *int    `mapstructure:"age"`
+		Name *string `mapstructure:"NAME"`
+		Age  *int    `mapstructure:"AGE"`
 	}
 
 	type TimeFieldConfig struct {
-		Birthday time.Time `mapstructure:"birthday"`
+		Birthday time.Time `mapstructure:"BIRTHDAY"`
 	}
 
 	nonStructConfig := "notastruct"
@@ -40,23 +41,64 @@ func TestParse(t *testing.T) {
 		name          string
 		input         interface{}
 		defaultConfig map[string]interface{}
+		envConfig     map[string]interface{}
 		expected      interface{}
 		expectError   bool
 	}{
 		{
-			name:  "simple config",
+			name:  "simple config vals from defaults",
 			input: &SimpleConfig{},
 			defaultConfig: map[string]interface{}{
-				"name":   "cuc",
-				"age":    79,
-				"active": false,
-				"height": 10.1,
+				"NAME":   "cuc",
+				"AGE":    79,
+				"ACTIVE": false,
+				"HEIGHT": 10.1,
 			},
 			expected: &SimpleConfig{
 				Name:   "cuc",
 				Age:    79,
 				Active: false,
 				Height: 10.1,
+			},
+			expectError: false,
+		},
+		{
+			name:  "simple config vals from env",
+			input: &SimpleConfig{},
+			envConfig: map[string]interface{}{
+				"NAME":   "cuc",
+				"AGE":    79,
+				"ACTIVE": false,
+				"HEIGHT": 10.1,
+			},
+			expected: &SimpleConfig{
+				Name:   "cuc",
+				Age:    79,
+				Active: false,
+				Height: 10.1,
+			},
+			expectError: false,
+		},
+		{
+			name:  "simple config with defaults and vals from env",
+			input: &SimpleConfig{},
+			defaultConfig: map[string]interface{}{
+				"NAME":   "cuc",
+				"AGE":    79,
+				"ACTIVE": false,
+				"HEIGHT": 10.1,
+			},
+			envConfig: map[string]interface{}{
+				"NAME":   "cucu",
+				"AGE":    22,
+				"ACTIVE": true,
+				"HEIGHT": 5.4,
+			},
+			expected: &SimpleConfig{
+				Name:   "cucu",
+				Age:    22,
+				Active: true,
+				Height: 5.4,
 			},
 			expectError: false,
 		},
@@ -91,6 +133,10 @@ func TestParse(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			for key, value := range tc.defaultConfig {
 				gonfiguration.SetDefault(gonfiguration.Default{Key: key, Value: value})
+			}
+
+			for key, value := range tc.envConfig {
+				t.Setenv(key, fmt.Sprintf("%v", value))
 			}
 
 			err := gonfiguration.Parse(tc.input)
